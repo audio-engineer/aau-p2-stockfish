f84f593fb0fabfca8d6bda7163c59c4b2d35dfdf
Martin Kedmenec <martin.kedmenec@outlook.com>
2024-03-19 15:28:09 +0100

    Fix #5. Add response for robots.txt

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
 from stockfish import Stockfish
+from fastapi.responses import PlainTextResponse
 
 app = FastAPI()
 stockfish = Stockfish()
+
+
+@app.get("/robots.txt", response_class=PlainTextResponse)
+def robots():
+    with open("robots.txt", encoding="utf-8") as f:
+        return f.read()
+
+
+@app.get("/about", response_class=PlainTextResponse)
+def robots():
+    with open("/usr/local/bin/COPYING", encoding="utf-8") as f:
+        return f.read()
 
 
 @app.get("/")
